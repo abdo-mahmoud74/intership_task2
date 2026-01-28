a631ed210b4022a7530407790483d3833028f7ae
Abdelrahman Mahmoud <73645524+abdo-mahmoud74@users.noreply.github.com>
2026-01-28 12:09:38 +0200

    craete comment migration and model

---
+++ app/Models/Comment.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+    protected $fillable = ['body', 'task_id', 'user_id'];
+    // A comment belongs to a task
+    public function task()
+    {
+        return $this->belongsTo(Task::class);
+    }
+    // A comment belongs to a user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
